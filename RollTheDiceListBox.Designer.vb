35f3da7fcaa8a27edc113f690d633ee5068ce03b
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-03-17 08:16:17 -0600

    Add project files.

---
+++ RollTheDiceListBox.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RollTheDiceListBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(123, 282)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(174, 111)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "&ROLL"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 16
+        Me.DisplayListBox.Location = New System.Drawing.Point(190, 91)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(437, 116)
+        Me.DisplayListBox.TabIndex = 1
+        '
+        'ClearButton
+        '
+        Me.ClearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ClearButton.Location = New System.Drawing.Point(312, 282)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(174, 111)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(498, 282)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(174, 111)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'RollTheDiceListBox
+        '
+        Me.AcceptButton = Me.RollButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ClearButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.RollButton)
+        Me.Name = "RollTheDiceListBox"
+        Me.Text = "Roll the Dice"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RollButton As Button
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+End Class
---
+++ RollTheDiceListBox.vb
@@ -0,0 +1,107 @@
+﻿
+Option Strict On
+Option Explicit On
+'Sean Gingerich
+'RCET0265
+'Spring 2021
+'Roll the Dice with List Box
+'https://github.com/gingsean5/RolltheDiceListBox
+
+Public Class RollTheDiceListBox
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim banana As Integer
+        Dim RollCount As String
+        RollCount = AddRandomNumbers()
+        If DisplayListBox.Items.Count = 0 Then
+
+
+            banana = CInt((77 - 14) / 2)
+            'Header that reads Random Numbers in the middle
+            DisplayListBox.Items.Add(StrDup(banana, " ") & "Random Numbers" & StrDup(banana, " "))
+            'line of - 
+            DisplayListBox.Items.Add(StrDup(100, "-"))
+            'Just spaces out numbers 2 to 12
+            'For i = 2 To 12
+            'Next
+            DisplayListBox.Items.Add(Str(2).PadLeft(6) & "|" _
+                                 & Str(3).PadLeft(6) & "|" _
+                                 & Str(3).PadLeft(6) & "|" _
+                                 & Str(4).PadLeft(6) & "|" _
+                                 & Str(5).PadLeft(6) & "|" _
+                                 & Str(6).PadLeft(6) & "|" _
+                                 & Str(7).PadLeft(6) & "|" _
+                                 & Str(8).PadLeft(6) & "|" _
+                                 & Str(9).PadLeft(6) & "|" _
+                                 & Str(10).PadLeft(6) & "|" _
+                                 & Str(11).PadLeft(6) & "|" _
+                                 & Str(12).PadLeft(6) & "|")
+
+            'line of -
+            DisplayListBox.Items.Add(StrDup(100, "-"))
+
+            DisplayListBox.Items.Add(RollCount)
+        Else
+            DisplayListBox.Items.Clear()
+            banana = CInt((77 - 14) / 2)
+            'Header that reads Random Numbers in the middle
+            DisplayListBox.Items.Add(StrDup(banana, " ") & "Random Numbers" & StrDup(banana, " "))
+            'line of - 
+            DisplayListBox.Items.Add(StrDup(100, "-"))
+            'Just spaces out numbers 2 to 12
+            'For i = 2 To 12
+            'Next
+            DisplayListBox.Items.Add(Str(2).PadLeft(6) & "|" _
+                                 & Str(3).PadLeft(6) & "|" _
+                                 & Str(3).PadLeft(6) & "|" _
+                                 & Str(4).PadLeft(6) & "|" _
+                                 & Str(5).PadLeft(6) & "|" _
+                                 & Str(6).PadLeft(6) & "|" _
+                                 & Str(7).PadLeft(6) & "|" _
+                                 & Str(8).PadLeft(6) & "|" _
+                                 & Str(9).PadLeft(6) & "|" _
+                                 & Str(10).PadLeft(6) & "|" _
+                                 & Str(11).PadLeft(6) & "|" _
+                                 & Str(12).PadLeft(6) & "|")
+
+            'line of -
+            DisplayListBox.Items.Add(StrDup(100, "-"))
+
+            DisplayListBox.Items.Add(RollCount)
+
+        End If
+    End Sub
+
+    Function AddRandomNumbers() As String
+        Dim totalRoll As Integer
+        Dim randomNumbers(12) As Integer
+        Dim ReturnRandos As String
+        For i = 0 To 1000
+            totalRoll = (MakeRandoNumber(5) + 1) + (MakeRandoNumber(5) + 1)
+            randomNumbers(totalRoll) += 1
+        Next
+        ReturnRandos = (CStr(randomNumbers(2)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(3)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(4)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(5)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(6)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(7)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(8)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(9)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(10)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(11)).PadLeft(6) & "|" _
+                                     & CStr(randomNumbers(12)).PadLeft(6) & "|")
+        Return ReturnRandos
+    End Function
+    Function MakeRandoNumber(maxNumber As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
+    End Function
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DisplayListBox.Items.Clear()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
